445e97c4c633476de838bb247030196b327f8ece
Zulfiqor0912 <zulfiqorrustamov81@gmail.com>
2026-05-06 15:25:28 +0500

    Add project files.

---
+++ Algorithm/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        var son = int.Parse(Console.ReadLine());
+        
+
+        Program p = new Program();
+        //Console.WriteLine(p.Task1(son));
+        //Console.WriteLine(p.Task2(son));
+    }
+    public int Task1(int a) => a * 5;
+    public int Task2(int a) => (int)Math.Pow(a, 2);
+}
